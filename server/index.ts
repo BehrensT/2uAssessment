54c81f1ca00c4931fb6feb772e9eee23ac4a89ab
Tom <BehrensT@gmail.com>
2023-01-29 16:47:00 -0500

    Initial Commit. Happy Path Server

---
+++ server/index.ts
@@ -0,0 +1,32 @@
+const bodyParser = require("body-parser");
+import "reflect-metadata";
+import express from 'express';
+import cors from 'cors';
+import Container from 'typedi';
+import InvoiceController from './Controller/InvoiceController';
+
+
+const main = async () => {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    const _invoiceController = Container.get(InvoiceController);
+
+
+    app.post('/invoice', (req, res) => _invoiceController.SaveInvoice(req, res));
+
+    app.get('/invoice/:id', (req, res) => _invoiceController.GetInvoiceById(req, res));
+
+    app.get('/invoice/:status/status', (req, res) => _invoiceController.GetInvoicesByStatus(req, res));
+
+
+    app.listen(4001, () => {
+        console.log("Listening on 4001");
+    });
+
+}
+
+main().catch(err => {
+    console.error(err);
+});
